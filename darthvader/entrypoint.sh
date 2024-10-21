393a2ca9d34a83072eb22987e331f4e9e8b274cd
a24adriansr <a24adriansr@iessanclemente.net>
2024-10-21 11:55:56 +0200

    dockerfile test

--- darthvader/entrypoint.sh
+++ darthvader/entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Comentar la carga del módulo imklog en rsyslog.conf
 sed -i 's/^module(load="imklog")/#module(load="imklog")/' /etc/rsyslog.conf
 
+
+# Iniciar rsyslog
 rsyslogd
+echo "iniciando named"
 
+
+# Iniciar BIND (named)
 /etc/init.d/named start
-
+echo "mostrando logs" 
+# Hacer tail de un archivo de log temporal en lugar de syslog
 tail -f /var/log/syslog
